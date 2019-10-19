7730769cb859012c682beeca1018c00b67e76af7
Danilo Marques <danilomarques20@hotmail.com>
2019-10-19 11:39:49 -0300

    Adicionando o navbar a pagina de login

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 import {
-    Nav
+    Nav,
 } from './styles';
 
 import Logo from '../../assets/logo.png';
@@ -9,7 +9,17 @@ import Logo from '../../assets/logo.png';
 export default function Navbar() {
     return (
         <Nav>
-            <img src={ Logo } alt='Logo' />
+            <img src={ Logo } alt='Logo' />   
+            
+            <span>Reddit</span>
+            <ul>
+                <li>
+                    <Link to='/register'>Sign up</Link>
+                </li>
+                <li>
+                    <Link to='/login'>Sign in</Link>
+                </li>
+            </ul>
         </Nav>
     )
 }
--- frontend/src/components/Navbar/styles.js
+++ frontend/src/components/Navbar/styles.js
@@ -1,10 +1,34 @@
 import styled from 'styled-components';
 
 export const Nav = styled.nav `
+    display: flex;
     background-color: #fff;
+    padding: 8px;
+    span {
+        align-self: center;
+    }
     img {
         width: 30px;
         height: 30px;
+        margin: 4px;
     }
-    padding: 8px;
+    ul {
+        display: flex;
+        list-style: none;
+        align-self: center;
+        margin-left: auto;
+    }
+
+    ul>li {
+        margin: 0 10px;
+    }
+
+    ul>li:hover {
+        border-bottom: 1px solid #3565B0;
+        cursor: pointer;
+    }
+    ul>li>a {
+        text-decoration: none;
+        color: #000;
+    }
 `
